cf1fcc3848dc8745786de9a889725157854f2fb3
xiaolu-coding <454819375@qq.com>
2023-01-10 14:19:12 +0800

    chore: 入口HTML加载

--- src/node/server/index.ts
+++ src/node/server/index.ts
@@ -7,6 +7,7 @@ import { optimize } from "../optimizer/index"
 import { resolvePlugins } from "../plugins"
 import { Plugin } from "../plugin"
 import { createPluginContainer, PluginContainer } from "../pluginContainer"
+import { indexHtmlMiddware } from "./middlewares/indexHtml"
 
 // 增加如下类型声明
 export interface ServerContext {
@@ -34,6 +35,9 @@ export async function startDevServer() {
       await plugin.configureServer(serverContext)
     }
   }
+
+  // 处理入口 HTML 资源
+  app.use(indexHtmlMiddware(serverContext))
 
   app.listen(3000, async () => {
     await optimize(root)
